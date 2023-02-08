b64d2f0574ad8d246e5e082b55a112b1e7a87f8f
Alex <alex_15_zhen@hotmail.com>
2023-02-08 02:27:47 -0500

    Initial Commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Alex Zhen on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
